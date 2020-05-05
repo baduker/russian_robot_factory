357a553e95af3402887e6c7d182ff95f00d058dc
baduker <baduker@protonmail.ch>
2020-05-05 15:22:49 +0200

    Added eyes and mouth replacer

--- russian_robot_factory.py
+++ russian_robot_factory.py
@@ -6,12 +6,19 @@ def random_hex_id() -> str:
     return ''.join(random.choice('0123456789abcdef') for _ in range(5))
 
 
+HEX_ID = random_hex_id()
+
+
+def load_parts(module: str):
+    return json.loads(open("robot_parts.json").read())[module]
+
+
 def random_robot() -> list:
-    robot_parts = json.loads(open("robot_parts.json").read())
+    robot_parts = load_parts("templates")
     robot = [
-        robot_parts[random_hex_id()[1]].split("\n")[:3],  # :3 -> HEAD
-        robot_parts[random_hex_id()[2]].split("\n")[3:5],  # 3:5 -> TRUNK
-        robot_parts[random_hex_id()[3]].split("\n")[5:7],  # 5:7 -> LEGS
+        robot_parts[HEX_ID[0]].split("\n")[:3],  # :3 -> HEAD
+        robot_parts[HEX_ID[1]].split("\n")[3:5],  # 3:5 -> TRUNK
+        robot_parts[HEX_ID[2]].split("\n")[5:7],  # 5:7 -> LEGS
     ]
     return [part for module in robot for part in module]
 
@@ -42,6 +49,18 @@ def build_robot(no_label: bool = False) -> str:
         return robot
 
     return f"{robot}\n{label_robot()}"
+
+
+def replacer(body: list):
+    eyes = 1
+    new_eyes = load_parts('eyes')[HEX_ID[3]]
+    mouth = 2
+    new_mouth = load_parts('mouths')[HEX_ID[4]]
+    # swap eyes
+    body[eyes] = body[eyes][:6] + new_eyes + body[eyes][6+len(new_eyes):]
+    # swap mouth
+    body[mouth] = body[mouth][:7] + new_mouth + body[mouth][7+len(new_mouth):]
+    return body
 
 
 if __name__ == "__main__":
